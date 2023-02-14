1dd1730359cc8eb7cff4c31eba27bad6ea032286
lipohan <a0966474739@gmail.com>
2023-02-14 20:49:36 +0800

    test

--- 1/Program.cs
+++ 1/Program.cs
@@ -14,17 +14,20 @@ namespace _1
         static void Main(string[] args)
         {
             char ans;
+            string str;
+            string[] one = new string[4];
+            int[] strs = new int[4];
             do
             {
                 
-                int[] one = new int[100];
-                int[] strs= new int[100];
+                
+                
                 int sumA = 0;
                 int sumB = 0;
                 Random random = new Random();
                 for (int i = 0; i < 4; i++)
                 {
-                    one[i] = random.Next(0, 10);
+                    one[i] = random.Next(0, 10).ToString();
                     for(int k = 0; k < i; k++)
                     {
                         if (one[i] == one[k])
@@ -45,34 +48,36 @@ namespace _1
                     sumB = 0;
                     Console.WriteLine("-------------------------\n");
                     Console.Write("請輸入 4 個數字：");
-                    string str = Console.ReadLine();
+
+                    str = Console.ReadLine();
+                   
                     //text
-                    for(int i = 0; i < 4; i++)
-                    {
-                        strs[i] = Convert.ToInt32(str[i]) - '0';
-                    }
                     for (int i = 0; i < 4; i++)
                     {
                         for (int j = 0; j < str.Length; j++)
                         {
                             
-                            if ((int)one[i] == (int)strs[j] && (int)one[i] != (int)strs[i])
+                            if (one[i] == str[j].ToString() && one[i] != str[i].ToString())
                             {
                                 sumB++;
                             }
                         }
-                        if ((int)one[i] == (int)strs[i])
+                        if (one[i] == str[i].ToString())
                         {
                             sumA++;
                         }
 
                     }
+                    
                     Console.WriteLine($"判定結果是 {sumA}A  {sumB}B \n");
                     
                 } while (sumA < 4);
                 Console.WriteLine("恭喜你！猜對了！！\r\n");
                 Console.WriteLine("你要繼續玩嗎？(y/n): ");
                 ans = (char)Console.Read();
+                Array.Clear(strs, 0, strs.Length);
+                Array.Clear(one, 0, one.Length);
+                str= Console.ReadLine();
             } while (ans == 'y');
             
             Console.WriteLine("遊戲結束，下次再來玩喔～\r\n");
